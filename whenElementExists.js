37c66f1aad63bad9649b7d1c9970e36238b985cf
Moritz <moritz@mailmmc.com>
2018-03-26 14:27:50 -0500

    updated in-code documentation

--- whenElementExists.js
+++ whenElementExists.js
@@ -1,12 +1,19 @@
 /**
+ * 
+ */
+
+
+/**
  * Listen until $selector exists, then runs callback function
+ * 
+ * @param  {jQuery Selector} selector - This is the selector you are waiting for to exist
+ * @param  {Function} callback - This is the function that runs when it exists
  */
 var whenElementExists = function(selector, callback){
-    var intervalId = setInterval(function() {
-      // $internalSelector = $(selector);
-      if ( $(selector.selector).length > 0 ){
-        clearInterval(intervalId);
-        callback();
-      }
-    }, 250);
+  var intervalId = setInterval(function() {
+    if ( $(selector.selector).length > 0 ){
+      clearInterval(intervalId);
+      callback();
+    }
+  }, 250);
 };
